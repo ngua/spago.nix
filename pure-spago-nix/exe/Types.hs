f72a7d158373e259841450f84244e85339c93995
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-08-06 20:42:11 +0700

    Basic Nix exprs with pretty-printing

--- pure-spago-nix/exe/Types.hs
+++ pure-spago-nix/exe/Types.hs
@@ -1,15 +1,25 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Types (
   SpagoDependencies (SpagoDependencies, imports),
   SpagoImport (SpagoImport, path, sha256),
   SpagoAddition (SpagoAddition, name, repo, version),
   emptyDependencies,
+  NixExpr (..),
+  prettyNixExpr,
 ) where
 
 import Data.Generics.Labels ()
+import Data.Kind (Type)
+import Data.Map qualified as Map
+import Data.Map.Strict (Map)
 import Data.Sequence (Seq)
 import Data.Text (Text)
 import Dhall.Crypto (SHA256Digest)
+import GHC.Exts (toList)
 import GHC.Generics (Generic)
+import Prettyprinter ((<+>))
+import Prettyprinter qualified
 
 {- | The dependencies declared in `packages.dhall`. Includes Dhall imports
  as well as additional git dependencies
@@ -18,7 +28,7 @@ data SpagoDependencies = SpagoDependencies
   { imports :: Seq SpagoImport
   , additions :: Seq SpagoAddition
   }
-  deriving stock (Generic, Eq, Show)
+  deriving stock (Show, Eq, Generic)
 
 emptyDependencies :: SpagoDependencies
 emptyDependencies = SpagoDependencies mempty mempty
@@ -30,7 +40,7 @@ data SpagoImport = SpagoImport
   { path :: Text
   , sha256 :: SHA256Digest
   }
-  deriving stock (Generic, Eq, Show)
+  deriving stock (Show, Eq, Generic)
 
 {- | An additional Spago dependency. We will check these against the sha256
  hash provided by the user
@@ -40,4 +50,40 @@ data SpagoAddition = SpagoAddition
   , repo :: Text
   , version :: Text
   }
-  deriving stock (Generic, Eq, Show)
+  deriving stock (Show, Eq, Generic)
+
+{- | The `hnix` package is currently broken on `nixpkgs`, so we need to define
+ our own (extremely limited) set of Nix expressions. We only care about creating
+very primitive literals -- just lists and attribute sets
+-}
+data NixExpr
+  = NixList (Seq NixExpr)
+  | NixAttrSet (Map Text NixExpr)
+  | NixString Text
+  deriving stock (Show, Eq, Generic)
+
+prettyNixExpr :: forall (ann :: Type). NixExpr -> Prettyprinter.Doc ann
+prettyNixExpr = \case
+  NixString t -> Prettyprinter.dquotes $ Prettyprinter.pretty t
+  NixList nexprs -> prettyElems ("[", "]") prettyNixExpr $ toList nexprs
+  NixAttrSet attrs -> prettyElems ("{", "}") f $ Map.toList attrs
+    where
+      f :: (Text, NixExpr) -> Prettyprinter.Doc ann
+      f (k, nexpr) =
+        Prettyprinter.pretty k
+          <+> "="
+          <+> prettyNixExpr nexpr <> ";"
+
+prettyElems ::
+  forall (a :: Type) (ann :: Type).
+  (Prettyprinter.Doc ann, Prettyprinter.Doc ann) ->
+  (a -> Prettyprinter.Doc ann) ->
+  [a] ->
+  Prettyprinter.Doc ann
+prettyElems (open, close) f xs =
+  Prettyprinter.sep
+    [ Prettyprinter.nest 2
+        . Prettyprinter.sep
+        $ [open] <> (f <$> xs)
+    , close
+    ]
