9394049b5cdaf3a5e1cd9310f078dcbfdb196709
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-08-20 17:47:19 +0700

    Use `legacyPackages` for faster evaluation

--- flake.nix
+++ flake.nix
@@ -13,15 +13,7 @@
     let
       perSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
       compiler = "ghc924";
-      nixpkgsFor = system: import nixpkgs {
-        inherit system;
-        overlays = nixpkgs.lib.attrValues self.overlays ++
-          [
-            (_: _: {
-              pure-spago-nix = self.packages.${system}.pure-spago-nix;
-            })
-          ];
-      };
+      nixpkgsFor = system: nixpkgs.legacyPackages.${system};
       hsPackageName = "pure-spago-nix";
       hsProjectFor = system: { returnShellEnv ? false }:
         (nixpkgsFor system).haskell.packages.${compiler}.developPackage {
@@ -37,24 +29,22 @@
 
       devShell = perSystem (system:
         ((hsProjectFor system) { returnShellEnv = true; }).overrideAttrs
-          (oas:
-            let
-              pkgs = nixpkgsFor system;
-            in
-            {
-              buildInputs = oas.buildInputs ++
-                (with pkgs.haskell.packages.${compiler}; [
-                  fourmolu
-                  haskell-language-server
-                  cabal-install
-                  hlint
-                  pkgs.wget
-                  pkgs.nixpkgs-fmt
-                  pkgs.pure-spago-nix
-                  pkgs.dhall-nix
-                  pkgs.nix-prefetch-git
-                ]);
-            })
+          (oas: {
+            buildInputs =
+              with (nixpkgsFor system);
+              with haskell.packages.${compiler};
+              [
+                fourmolu
+                haskell-language-server
+                cabal-install
+                hlint
+                wget
+                nixpkgs-fmt
+                nix-prefetch-git
+                self.packages.${system}.pure-spago-nix
+              ]
+              ++ oas.buildInputs;
+          })
       );
 
       overlays = {
