63719b40a3639abbba7db0a15bef774f7147a9ea
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-08-28 11:56:51 +0700

    Reuse the fake Spago env and `packages.dhall`

--- lib/project.nix
+++ lib/project.nix
@@ -117,6 +117,50 @@ let
       }
     );
 
+  # The following two are necessary to prevent both Dhall and Spago from
+  # attempting network connections. The idea is:
+  #   - copy the installed Spago packages to a fake `XDG_CACHE_HOME` location
+  #   - also copy a metadata file that Spago generates there
+  #   - download the upstream package set (ideally, this should be done
+  #     elsewhere)
+  #   - reconstruct a `packages.dhall` that does not contain any remote
+  #     imports, by combining the raw Dhall additions along with the resolved
+  #     upstream import (i.e. the downloaded Dhall file)
+  prepareFakeSpagoEnv = ''
+    export HOME="$TMP"
+    mkdir -p "$HOME"/.cache/spago
+    export XDG_CACHE_HOME="$HOME"/.cache
+    cp -r ${cached}/* "$XDG_CACHE_HOME"/spago
+    cp ${self}/lib/metadataV1.json "$XDG_CACHE_HOME"/spago
+  '';
+
+  fakePackagesDhall =
+    let
+      packageSetUrl =
+        "https://github.com/purescript/package-sets/releases/download";
+      packageSetDhall = builtins.fetchurl {
+        # FIXME
+        # This should be generated automatically when making the upstream
+        # package sets
+        sha256 = (import ./upstream-hashes.nix).${plan.upstream.path};
+        url = "${packageSetUrl}/${plan.upstream.path}/packages.dhall";
+      };
+    in
+    pkgs.runCommand
+      "fake-packages-dhall"
+      {
+        buildInputs = [ pkgs.dhall ];
+      }
+      ''
+        cat <<EOF >additions.dhall
+          ${plan.additions-dhall}
+        EOF
+        upstream=$(dhall --file ${packageSetDhall})
+        additions=$(dhall --file ./additions.dhall)
+        dhall <<< "$upstream // $additions" > ./packages.dhall
+        mkdir $out && mv ./packages.dhall $out
+      '';
+
   # Unfortunately, we need both of these package sets, and there's a bit
   # of unavoidable waste between them
   #
@@ -136,48 +180,19 @@ let
 
   # This is a necessary step to get the exact project dependencies, including
   # the transitive dependencies. Otherwise, we will always build the entire
-  # upstream package set during compilation. The idea is to
-  #   - copy the installed Spago packages to a fake `XDG_CACHE_HOME` location
-  #   - also copy a metadata file that Spago generates there
-  #   - download the upstream package set (ideally, this should be done
-  #     elsewhere)
-  #   - reconstruct a `packages.dhall` that does not contain any remote
-  #     imports, by combining the raw Dhall additions along with the resolved
-  #     upstream import (i.e. the downloaded Dhall file)
-  # All of this is necessary to prevent both Dhall and Spago from attempting
-  # network connections
+  # upstream package set during compilation
   lsDeps = import
     (
-      let
-        packageSetUrl =
-          "https://github.com/purescript/package-sets/releases/download";
-        packageSetDhall = builtins.fetchurl {
-          # FIXME
-          # This should be generated automatically when making the upstream
-          # package sets
-          sha256 = (import ./upstream-hashes.nix).${plan.upstream.path};
-          url = "${packageSetUrl}/${plan.upstream.path}/packages.dhall";
-        };
-      in
       pkgs.runCommand "${name}-ls-deps"
         {
-          buildInputs = [ eps.spago pkgs.dhall pkgs.jq ];
+          buildInputs = [ eps.spago pkgs.jq fakePackagesDhall ];
         }
         ''
-          export HOME="$TMP"
-          mkdir -p "$HOME"/.cache/spago
-          export XDG_CACHE_HOME="$HOME"/.cache
-          cp -r ${cached}/* "$XDG_CACHE_HOME"/spago
-          cp ${self}/lib/metadataV1.json "$XDG_CACHE_HOME"/spago
+          ${prepareFakeSpagoEnv}
 
-          mkdir $out
-          cat <<EOF >additions.dhall
-            ${plan.additions-dhall}
-          EOF
-          upstream=$(dhall --file ${packageSetDhall})
-          additions=$(dhall --file ./additions.dhall)
-          dhall <<< "$upstream // $additions" > ./packages.dhall
           cp ${buildConfig.spagoDhall} ./spago.dhall
+          cp ${fakePackagesDhall}/packages.dhall .
+          mkdir $out
           spago ls deps -t --json |
             jq --slurp 'map(.packageName)' |
             tr ',' ' ' > $out/default.nix
