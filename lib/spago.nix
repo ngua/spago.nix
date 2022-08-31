40d66076e1c52e5f609671263e05df831ca0dfb7
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-08-31 13:05:28 +0700

    Add some utils for working with flakes

--- lib/spago.nix
+++ lib/spago.nix
@@ -5,4 +5,29 @@ let
     inherit self inputs pkgs;
   };
 in
-{ inherit spagoProject; }
+{
+  inherit spagoProject;
+
+  # Utilities for generating flake outputs, etc...
+  utils = {
+    apps =
+      let
+        appHelper = program: {
+          inherit program;
+          type = "app";
+        };
+      in
+      {
+        # Turn a NodeJS app (e.g. using `nodeApp` from `spagoProject`) into a
+        # flake app
+        fromNodeApp = app: appHelper "${app}/bin/${app.name}";
+
+        # Turn a NodeJS app (e.g. using `nodeApp` from `spagoProject`) into a
+        # flake app and run it with the arguments provided to `nix run`
+        #
+        # NOTE: You must include the command name as the first argument, e.g.
+        # `nix run -- cmd --arg1 --arg2 ...`
+        fromNodeAppWithArgs = app: appHelper ''${app}/bin/${app.name} "$@"'';
+      };
+  };
+}
