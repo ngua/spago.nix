04e3198d639deb984fc7f187f7ece3be227a5efa
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-09-01 13:11:06 +0700

    Add docs app

--- lib/spago.nix
+++ lib/spago.nix
@@ -23,6 +23,22 @@
         # NOTE: You must include the command name as the first argument, e.g.
         # `nix run -- cmd --arg1 --arg2 ...`
         fromNodeAppWithArgs = app: appHelper ''${app}/bin/${app.name} "$@"'';
+
+        fromDocs = { docs, port ? 8080 }:
+          let
+            server = pkgs.writeShellApplication {
+              name = "docs-server";
+              runtimeInputs = [
+                pkgs.nodejs-14_x
+                pkgs.nodePackages.http-server
+              ];
+              text =
+                ''
+                  http-server --port ${builtins.toString port} ${docs}/generated-docs
+                '';
+            };
+          in
+          appHelper "${server}/bin/${server.name}";
       };
   };
 }
